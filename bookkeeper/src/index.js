ddf6e627c8986648944ad6a48503504f5bd8e4a0
FengTing Guo <15019668+TrueFengTingGuo@users.noreply.github.com>
2020-07-29 14:56:53 -0400

    basic functionality finished
    1. added history for recent used word (by stringSimilarity)
    2. records can now be displayed (sort by date)
    3. total cost can now be displayed

--- bookkeeper/src/index.js
+++ bookkeeper/src/index.js
@@ -10,8 +10,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences:{
       nodeIntegration:true, // this can access node js directly in the code
     }
--- bookkeeper/src/render.js
+++ bookkeeper/src/render.js
@@ -1,10 +1,15 @@
 const addBtn = document.getElementById('addBtn');
 const namingBar = document.getElementById('namingBar');
 const categoryBar = document.getElementById('categoryBar');
+const DateBar = document.getElementById('DateBar');
 const moneyBar = document.getElementById('moneyBar');
 const suggestionContainer = document.getElementById('suggestionContainer');
+const recordContainer = document.getElementById('recordContainer');
+const totalCostDispplay = document.getElementById('totalCost');
+
 const fs = require('fs');
 const readline = require('readline');
+const stringSimilarity = require('string-similarity');
 const dataStoreSystem = require('./storeRecordData.js');
 
 
@@ -17,8 +22,7 @@ const recordStoreSystem = new dataStoreSystem({
     defaults: {
         record:[
             //some stored examples
-                {"name":"restaurant", "categoryBar":"food"},
-                {"name":"toy", "categoryBar":"toy"}
+                {"name":"restaurant", "date": "1","category":"food", "money":"0"},
             ]
     }
   });
@@ -26,54 +30,113 @@ const recordStoreSystem = new dataStoreSystem({
   
 window.onload = ()=>{
 
-
+    displayRecord();
     //adding quick searching for name of the record
     namingBar.addEventListener('input',()=>{
-        giveSuggestionforRecordNaming(namingBar.value);
+        giveSuggestionforNaming(namingBar.value,'name');
 
     });
 
     categoryBar.addEventListener('input',()=>{
-        giveSuggestionforCategoryNaming(categoryBar.value);
+        giveSuggestionforNaming(categoryBar.value,'category');
 
     });
 
 };
 
-
+//when add button is pressed
 addBtn.onclick = e => {
 
-    recordStoreSystem.addToArray('record',  {"name":namingBar.value, "categoryBar":categoryBar.value});
-
+    if(namingBar.value !== '' && DateBar.value !== '' && categoryBar !== '' && moneyBar.value!== ''){
+        recordStoreSystem.addToArray('record',  {"name":namingBar.value, "date":DateBar.value, "category":categoryBar.value, "money":moneyBar.value});
+        displayRecord();
+        console.log("added successfully");
+    }
 };
 
 
 //this function will put suggestions to the suggestionContainer div
-function giveSuggestionforRecordNaming(inputName){
-
+function giveSuggestionforNaming(inputName,sectionToLook){
 
     let recentRecord = recordStoreSystem.get('record');
-
-    console.log(recentRecord);
-    suggestionContainer.innerHTML = '';
-
-    for (i = 0; i < recentRecord.length; i++) {
-        suggestionContainer.innerHTML += `<div class="suggestionBlock"><button id="suggestionUseBtn">${recentRecord[i].name}</button></div>`;
-      }
-
-
+    let sortable = [];
     
 
+    //empty the html eveytime use is typing
+    suggestionContainer.innerHTML = '';
+
+    //sorting record with order (string Similarity)
+    for (i = 0; i < recentRecord.length; i++) {
+
+        let similarity = stringSimilarity.compareTwoStrings(inputName, recentRecord[i][sectionToLook]); 
+        
+        if(similarity >= 0.3){
+            
+            sortable.push([recentRecord[i].name,similarity]);
+        }
+        
+    }
+
+    sortable.sort(function(a, b) {
+        return a[1] - b[1];
+    });
+
+    for (i = 0; i < sortable.length; i++) {
+
+        suggestionContainer.innerHTML += `<div class="suggestionBlock"><button id="suggestionUseBtn">${sortable[i][0]}</button></div>`;
+    }
+
+
 
 }
 
- 
+//display all record in the window
+function displayRecord(){
 
-//this function will put suggestions to the suggestionContainer div
-function giveSuggestionforCategoryNaming(inputName){
+    let recentRecord = recordStoreSystem.get('record');
+    let sortable = [];
+    let totalCost = 0;
 
+    recordContainer.innerHTML = '';
 
+    //sorting record with order (string Similarity)
+    for (i = 0; i < recentRecord.length; i++) {
+
+            //calculate the total money
+            totalCost += Number(recentRecord[i].money);
+
+            //sort the record by date
+            sortable.push([recentRecord[i].name,recentRecord[i].date,recentRecord[i].category,recentRecord[i].money]);
+        
+    }
+
+    //comparsion function for date
+    sortable.sort(function(a, b) {
+
+        let d1 = new Date(a[1]);
+        let d2 = new Date(b[1]);
+        if(d1 < d2){
+            return 1;
+        }
+        if(d1 > d2){
+            return -1;
+        }
+        if(d1 = d2){
+            return 0;
+        }
+    });
+
+    //put all record in the container (in order)
+    for (i = 0; i < sortable.length; i++) {
+
+        recordContainer.innerHTML += 
+            `<div class="single_record">
+                <h1 class="record_description" style="font-size:2vw;">${sortable[i][0]}</h1>
+                <h1 class="record_date" style="font-size:2vw;">${sortable[i][1]}</h1>
+                <h1 class="record_amount" style="font-size:2vw;">${sortable[i][3]}</h1>
+                <button class="record_delete" id="Delete">Delete</button>
+            </div>`;
+    }
+
+    totalCostDispplay.innerHTML = `Total Cost: ${totalCost}`;
 }
-
-
-
